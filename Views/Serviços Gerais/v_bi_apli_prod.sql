f09144fcfcfe9fcbab66fa64ad009a8610e06aba
Yago da Silva Leme <143760057+YagoL3m3@users.noreply.github.com>
2025-06-10 15:46:30 -0300

    Alteração feita nesses relatórios

--- Views/Serviços Gerais/v_bi_apli_prod.sql
+++ Views/Serviços Gerais/v_bi_apli_prod.sql
@@ -1,22 +1,21 @@
--- public.v_bi_apli_prod source
-
-CREATE OR REPLACE VIEW public.v_bi_apli_prod
-AS select 
-	   oapl_emitent_1 as cd_empr,
-       oapl_produti_1 as cd_prod,
-       opro_nomepro_1 as nm_prod,
-       oapl_horaven_1 as qt_hven,
-       oapl_horapli_1 as qt_hapl,
-       oapl_datinve_1 as data_apli,
-       oapl_codativ_1 as codativ,
-       ntab_descric_76 as desc_ativ,
-       oapl_codserv_1 as codserv,
-       ords_descsv1_1 as desc_serv,
-       oapl_nrordem_1 as nr_os,
-       oapl_sequenc_1 as seque
- from apoapl a
- left join apopro on opro_produti_1 = a.oapl_produti_1
- left join sintab76 on ntab_codativ_76 = oapl_codativ_1
- join ordsrvs on oapl_emitent_1 = oapl_emitent_1 and  ords_nrordem_1 = oapl_nrordem_1 and ords_sequenc_1 = oapl_sequenc_1
-   WHERE a.oapl_produti_1 > 0
+/* HTML */
+SELECT U.SINC_NOMUSER_4 AS "Usuario*",
+       Q.TQLM_CODPROD_1 AS "Codigo Produto+",
+       E.TPRO_DESCRIC_1 AS "Descricao",
+       Q.TQLM_LOCACAO_1 AS "Locacao Origem",
+       Q.TQLM_LOCCORR_1 AS "Locacao Destino",         
+      COALESCE( CAST((select SUM(M.FIM - M.INICIO)
+        from WMS_MOVIMENTACAO_LOG M
+        where M.ID_PRODUTO = Q.tqlm_codprod_1
+          and cast(M.INICIO as date) BETWEEN  :DATA_INICIAL_D AND :DATA_FINAL_D           
+          and USUARIO = Q.tqlm_usuario_1
+          and M.id_locacao_destino = Q.tqlm_loccorr_1
+          and cast(lpad(cast(Q.tqlm_horamov_1 as text), 8, '0') as varchar(4)) = lpad(cast( extract(HOUR FROM M.FIM) as text), 2, '0') || lpad(cast(extract(MINUTE FROM M.FIM) as text), 2, '0')) AS TEXT), 'N/A')  AS "Tempo",      
+       Q.TQLM_QUANTID_1 AS "Quantidade$SUM"
+FROM ESTQLM Q
+INNER JOIN SINC04 U ON (Q.TQLM_USUARIO_1 = U.SINC_CODUSER_4) 
+INNER JOIN ESTPRO E ON (Q.TQLM_CODPROD_1 = E.TPRO_CODPROD_1)
+WHERE cast(Q.TQLM_DATAMOV_1 as date) BETWEEN :DATA_INICIAL_D AND :DATA_FINAL_D
+AND Q.TQLM_PROGRAM_1 = 'WMS'
+AND TQLM_TIPOMOV_1 = 'Saida'
 
